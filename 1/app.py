3fbf4b9ce3d1d18542f92176a7a43c5147a88b59
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-11 19:10:54 +0300

    Add files via upload

---
+++ 1/app.py
@@ -0,0 +1,15 @@
+# Terminalde çalıştırmanız gereken komutlar:
+#
+# 1. Streamlit kütüphanesini kurmak için (eğer daha önce kurmadıysanız):
+#    pip install streamlit
+#
+# 2. Bu uygulamayı çalıştırmak için bu dosyanın olduğu ana dizindeyken:
+#    streamlit run M5L1/Aşama_1/app.py
+
+import streamlit as st
+
+# Bu basit komut, ekranınıza eğlenceli bir animasyon getirecek.
+# Streamlit'in ne kadar kolay ve güçlü olduğunu gösteren harika bir başlangıç.
+st.balloons()
+
+st.write("Streamlit'e Hoş Geldiniz!")
---
+++ 2/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# st.title() kullanarak stüdyonuza bir isim verin.
+st.title("Pixel-Perfect Oyun Stüdyosu")
+
+# Şimdi st.header() kullanarak "Vizyonumuz" adında bir alt başlık ekleyin.
+# KODUNUZU AŞAĞIYA YAZIN
+
+
+# Son olarak, st.write() ve üç tırnak (""") kullanarak stüdyonuzun vizyonunu anlatan
+# çok satırlı bir metin ekleyin.
+# KODUNUZU AŞAĞIYA YAZIN
